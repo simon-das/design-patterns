5bc99f85998ffd94d15c8f0a9657dccab2080ab2
simon-das <sdsimondas@gmail.com>
2023-05-05 13:06:34 +0600

    Add factory python implementation

---
+++ factory/factory.py
@@ -0,0 +1,36 @@
+from abc import abstractmethod
+
+
+class Animal:
+    @abstractmethod
+    def speak(self):
+        pass
+
+
+class Dog(Animal):
+    def speak(self):
+        return 'Woof!'
+
+
+class Cat(Animal):
+    def speak(self):
+        return 'Meow!'
+
+
+class AnimalFactory:
+    @staticmethod
+    def create_animal(type):
+        if type == 'dog':
+            return Dog()
+        elif type == 'cat':
+            return Cat()
+        else:
+            raise ValueError('Invalid animal type')
+
+
+if __name__ == '__main__':
+    dog = AnimalFactory.create_animal('dog')
+    cat = AnimalFactory.create_animal('cat')
+
+    print(dog.speak()) # Output: Woof!
+    print(cat.speak()) # Output: Meow!
